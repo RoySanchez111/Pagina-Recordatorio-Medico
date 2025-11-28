955cb328779f131b700c61216c99b39dbf7bf4e7
Racoth505 <rafafl2001@gmail.com>
2025-11-28 08:41:30 -0600

    Logo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom'; // Importar
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import myLogo from "./assets/heartbeat.png";
+
+document.title = "Health Reminder";
+
+// Find the existing favicon link or create a new one
+let link = document.querySelector("link[rel~='icon']");
+if (!link) {
+  link = document.createElement("link");
+  link.rel = "icon";
+  document.head.appendChild(link);
+}
+link.href = myLogo;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* Envolver App con BrowserRouter */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
